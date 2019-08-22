0d3fbf612725a41654aa75bd7af315e31d6d864a
Akash <matthias@akash-desktop.localdomain>
2019-08-22 12:11:36 -0400

    utils to save the image after every epoch and generate gif

---
+++ utils.py
@@ -0,0 +1,67 @@
+import torch
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+import math
+import itertools
+import imageio
+import natsort
+from glob import glob
+
+def get_data_loader(root, batch_size):
+    
+    transform = transforms.Compose([
+        transforms.Resize(64),
+        transforms.CenterCrop(64),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    dataset = datasets.ImageFolder(root=root, transform=transform)
+
+    # Data Loader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+def generate_images(epoch, path, fixed_noise, num_test_samples, netG, device, use_fixed=False):
+    z = torch.randn(num_test_samples, 100, 1, 1, device=device)
+    size_figure_grid = int(math.sqrt(num_test_samples))
+    title = None
+  
+    if use_fixed:
+        generated_fake_images = netG(fixed_noise)
+        path += 'fixed_noise/'
+        title = 'Fixed Noise'
+    else:
+        generated_fake_images = netG(z)
+        path += 'variable_noise/'
+        title = 'Variable Noise'
+  
+    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(6,6))
+    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+        ax[i,j].get_xaxis().set_visible(False)
+        ax[i,j].get_yaxis().set_visible(False)
+    for k in range(num_test_samples):
+        i = k//4
+        j = k%4
+        ax[i,j].cla()
+        ax[i,j].imshow((generated_fake_images[k].cpu().data.numpy().transpose(1, 2, 0) + 1) / 2 )
+    label = 'Epoch_{}'.format(epoch+1)
+    fig.text(0.5, 0.04, label, ha='center')
+    fig.suptitle(title)
+    fig.savefig(path+label+'.png')
+
+def save_gif(path, fps, fixed_noise=False):
+    if fixed_noise==True:
+        path += 'fixed_noise/'
+    else:
+        path += 'variable_noise/'
+    images = glob(path + '*.png')
+    images = natsort.natsorted(images)
+    gif = []
+
+    for image in images:
+        gif.append(imageio.imread(image))
+    imageio.mimsave(path+'animated.gif', gif, fps=fps)
+
+    
+
+    
